b8791f408084aefe8149c2ad705123fce8400f2e
Antonio Sampaio Jr <assoftbel@gmail.com>
2022-11-21 15:39:13 -0300

    OK

--- src/components/GovEmployee/GovEmployeeDetails/GovEmployeeDetails.js
+++ src/components/GovEmployee/GovEmployeeDetails/GovEmployeeDetails.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import { Button, Card, Col, Container, ListGroup, Row } from "react-bootstrap"
 import { useNavigate, useParams } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAt, faPhone, faCalendarCheck, faCircleCheck, faBuildingUser} from "@fortawesome/free-solid-svg-icons";
@@ -23,7 +23,8 @@ function GovEmployeeDetails({ apiURL }) {
     }, [id])
    
     return (
-        <Container style={{ height: '80vh' }} className="d-flex justify-content-center align-items-center">
+        <Container style={{ height: '90vh' }} className="d-flex justify-content-center align-items-center">
+            
             <Card className="text-center w-100">
                 <Card.Header>
                     <Card.Title className="m-0">
@@ -38,58 +39,37 @@ function GovEmployeeDetails({ apiURL }) {
                             <Card.Img variant="top" src={employee.foto} />
                         </Col>
                         <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Matrícula: {employee.matricula}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Órgão: {employee.orgao}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Cargo: {employee.cargo}
-                            </Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item action variant="primary">Matrícula: {employee.matricula}</ListGroup.Item>
+                                <ListGroup.Item action variant="secondary">Órgão: {employee.orgao}</ListGroup.Item>
+                                <ListGroup.Item action variant="success">Cargo: {employee.cargo}</ListGroup.Item>
+                                <ListGroup.Item action variant="warning">Data de Admissão: {employee.dataAdmissao}</ListGroup.Item>
+                            </ListGroup>
                         </Col>
                         <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Lotação: {employee.lotacao}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faBuildingUser} /> Exercício: {employee.exercicio}
-                            </Card.Text>
-                        </Col>
-                        <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Vínculo: {employee.vinculo}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCalendarCheck} /> Data de Admissão: {employee.dataAdmissao}
-                            </Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item action variant="primary">Lotação: {employee.lotacao}</ListGroup.Item>
+                                <ListGroup.Item action variant="secondary">Exercício: {employee.exercicio}</ListGroup.Item>
+                                <ListGroup.Item action variant="success">Vínculo: {employee.vinculo}</ListGroup.Item>
+                            </ListGroup>
                         </Col>
                     </Row>
                     <Row>
                         <Card.Title>Informações Pessoais</Card.Title>
                         <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faAt} /> Email: {employee.email}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faPhone} /> Telefone: {employee.telefone}
-                            </Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item action variant="primary">Email: {employee.email}</ListGroup.Item>
+                                <ListGroup.Item action variant="secondary">Telefone: {employee.telefone}</ListGroup.Item>
+                                <ListGroup.Item action variant="success">Celular: {employee.celular}</ListGroup.Item>
+                            </ListGroup>
                         </Col>
                         <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faPhone} /> Celular: {employee.celular}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> CPF: {employee.cpf}
-                            </Card.Text>
-                        </Col>
-                        <Col>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCalendarCheck} /> Data de Nascimento: {employee.dataNascimento}
-                            </Card.Text>
-                            <Card.Text>
-                                <FontAwesomeIcon icon={faCircleCheck} /> Naturalidade: {employee.Naturalidade}
-                            </Card.Text>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item action variant="primary">CPF: {employee.cpf}</ListGroup.Item>
+                                <ListGroup.Item action variant="secondary">Data de Nascimento: {employee.dataNascimento}</ListGroup.Item>
+                                <ListGroup.Item action variant="success">Naturalidade: {employee.Naturalidade}</ListGroup.Item>
+                            </ListGroup>
+
                         </Col>
                     </Row>
                     <Row className="mt-3">
